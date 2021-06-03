41e3d1b06ec8dac8a4236845404b02f7b0db44e1
jfer2 <jfer2@pdx.edu>
2021-06-03 09:05:02 -0700

    Added map functionality

--- rlt/src/main.rs
+++ rlt/src/main.rs
@@ -1,99 +1,206 @@
-extern crate ncurses;
-use ncurses::*;
-use std::io::{self, Read};
+// This file is generated automatically. Do not edit it directly.
+// See the Contributing section in README on how to make changes to it.
+use tcod::colors::*;
+use tcod::console::*;
 
-const MAP_WIDTH: i32 = 140;
-const MAP_HEIGHT: i32 = 40;
+// actual size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
+type Map = Vec<Vec<Tile>>;
 
 struct Game {
     map: Map,
 }
 
+/// A tile of the map and its properties
 #[derive(Clone, Copy, Debug)]
 struct Tile {
-    ascii: char,
-    walkable: bool,
+    blocked: bool,
+    block_sight: bool,
 }
 
 impl Tile {
-    fn empty() -> Self {
+    pub fn empty() -> Self {
         Tile {
-            ascii: '.',
-            walkable: true,
+            blocked: false,
+            block_sight: false,
         }
     }
-    fn wall() -> Self {
+
+    pub fn wall() -> Self {
         Tile {
-            ascii: '#',
-            walkable: false,
+            blocked: true,
+            block_sight: true,
         }
     }
 }
 
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+#[derive(Debug)]
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
 
-type Map = Vec<Vec<Tile>>;
-
-fn make_map() -> Map {
-    let mut map: Vec<Vec<Tile>> =  Vec::new();
-
-    for _ in 0..=MAP_HEIGHT {
-        map.push(Vec::new());
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
     }
-    for x in 0..=MAP_HEIGHT {
-        for y in 0..=MAP_WIDTH {
-            if x == 0 || y == 0 || x == MAP_HEIGHT || y == MAP_WIDTH {
-                map[x as usize].push(Tile::wall());
-            } else {
-                map[x as usize].push(Tile::empty());
-            }
+
+    /// move by the given amount, if the destination is not blocked
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
         }
     }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // create walls around paramater
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
     map
 }
 
-fn render(game: Game) {
-    for y in 0..=MAP_HEIGHT {
-        for x in 0..=MAP_WIDTH {
-            let curr_tile = game.map[y as usize][x as usize];
-            print!("{}", curr_tile.ascii);
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
         }
-        println!("");
     }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    // blit the contents of "con" to the root console
+    blit(
+        &tcod.con,
+        (0, 0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
 }
 
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // exit game
+
+        // movement keys
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
+
+        _ => {}
+    }
+
+    false
+}
 
 fn main() {
-    /*
-    let mut game = Game {
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
+
+    let game = Game {
+        // generate map (at this point it's not drawn to the screen)
         map: make_map(),
     };
-    render(game);
-    */
 
-    /* Print to the back buffer. */
+    while !tcod.root.window_closed() {
+        // clear the screen of the previous frame
+        tcod.con.clear();
 
+        // render the screen
+        render_all(&mut tcod, &game, &objects);
 
-    let mut v: Vec<i32> = Vec::new();
-    let mut count = 0;
-        initscr();
-    while count < 5 {
-        /* Update the screen. */
-        refresh();
-        println!("hellllloooooo,world!!!"); 
+        tcod.root.flush();
 
-        /* Wait for a key press. */
-        let p = getch();
-        refresh();
-        v.push(p);
-        count += 1;
-        
-        /* Terminate ncurses. */
+        // handle keys and exit game if needed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, &game, player);
+        if exit {
+            break;
+        }
     }
-        endwin();
-    println!("{:?}", v);
-
-
-
-    
-
 }
+
